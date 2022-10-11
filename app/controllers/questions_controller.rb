a453a741e0bbecce293042b8d726aa92f64df400
Simona Barone <simona_barone@hotmail.com>
2022-10-11 20:29:15 +0200

    completed fist rails project

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,12 @@
+class QuestionsController < ApplicationController
+
+  def home
+  end
+
+  def ask
+
+  end
+
+  def answer
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,8 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  root to: 'pages#home'
+  get 'ask', to: 'pages#ask'
+  get 'answer', to: 'pages#answer'
 end
